ec39ba3cccf46305352280a0c6a729772eb529db
Mugisha Joyce-Christiane Kakou <mugisha.kakou@gmail.com>
2022-01-12 14:56:13 +0100

    Basic file architecture

---
+++ src/pages/conversation.tsx
@@ -0,0 +1,14 @@
+import type { FC } from 'react'
+import styles from '../styles/Conversation.module.css'
+
+const Conversation: FC = () => {
+  
+
+  return (
+    <div className={styles.container}>
+     Conversation screen
+    </div>
+  )
+}
+
+export default Conversation
---
+++ src/pages/message.tsx
@@ -0,0 +1,14 @@
+import type { FC } from 'react'
+import styles from '../styles/Message.module.css'
+
+const Message: FC = () => {
+  
+
+  return (
+    <div className={styles.container}>
+     Message screen
+    </div>
+  )
+}
+
+export default Message
